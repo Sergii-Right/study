9d81421e14bb1e15553e18bdfdfe3576821e1c92
sergii-right <sergii.ziborov@gmail.com>
2015-08-30 23:29:41 +0300

    19 e-olimp

--- e-olimp/19/main.cpp
+++ e-olimp/19/main.cpp
@@ -1,8 +1,30 @@
-#include <QCoreApplication>
+#include <stdio.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    char str[10];
+    short size = 0, sum = 0;
 
-    return a.exec();
+    scanf("%10s",&str);
+    for (int i = 0; i< 10; i++)
+    {
+        if (str[i])
+        {
+            size++;
+        }
+        else break;
+    }
+    if(size % 2 > 0)
+    {
+        sum++;
+    }
+    for (int i = 0, j = size-1; i < size/2; i++, j--)
+    {
+        if(str[i] == str[j])
+            sum++;
+    }
+
+    printf("%d",sum);
+
+    return 0;
 }
