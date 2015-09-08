0003b3d64c52be3b51d575f054627ddac9ed8dd2
sergii-right <sergii.ziborov@gmail.com>
2015-09-08 14:46:30 +0300

    some changes

---
+++ c#/с_wrarray/с_wrarray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace с_wrarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string a = Console.ReadLine();
+            //Console.WriteLine(a);
+            //Console.WriteLine(Console.ReadKey().Key);
+            //int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            //char[] arr = Console.ReadLine().ToCharArray();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //Console.WriteLine(arr[i]);
+            //}
+            char[] ar = Console.ReadLine().ToCharArray(0,2);
+            Console.WriteLine("{0} {1}",ar[0],ar[1]);
+        }
+    }
+}
---
+++ e-olimp/c#/e_1/e_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace e_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = Console.ReadLine().ToCharArray(0, 2);
+            Console.WriteLine("{0} {1}", arr[0], arr[1]);
+        }
+    }
+}
