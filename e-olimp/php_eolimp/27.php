1bed6bdf59159fefc6be8fed4a1a92608cc979f2
sergii-right <sergii.ziborov@gmail.com>
2015-09-14 13:20:47 +0300

    e-olimp php 27

--- e-olimp/php_eolimp/27.php
+++ e-olimp/php_eolimp/27.php
@@ -1,15 +1,18 @@
 <?php
-
 $b_str = decbin(intval(trim(fgets(STDIN))));
-echo $b_str;
 $length = strlen($b_str);
 $b_arr = str_split($b_str);
-$str_arr = [];
-for($i = 0, $j = $length - 1; $i < $length - 1; $i++, $j--)
+for($i = 0; $i < $length - 1; $i++)
 {
-    if($str[$j] == 1)
+    if($b_arr[$length - 1] == 1)
     {
         array_pop($b_arr);
-        array_unshift($b_str,1);
+        array_unshift($b_arr,1);
+    }
+    else
+    {
+        break;
     }
 }
+
+echo bindec(implode($b_arr));
